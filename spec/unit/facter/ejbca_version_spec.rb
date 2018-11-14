84549791139792685525ef26921b62d4de70476c
Gerard Kok <gerard@gerardkok.nl>
2018-11-14 10:23:26 +0100

    fixing test when run on its own

--- spec/unit/facter/ejbca_version_spec.rb
+++ spec/unit/facter/ejbca_version_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'facter/ejbca'
+require 'puppet_x/ejbcaws/client'
 
 describe 'Facter::Util::Fact' do
   before(:each) do
@@ -8,8 +8,8 @@ describe 'Facter::Util::Fact' do
 
   context 'ejbcaws accessible' do
     it do
-      allow(Puppet::Confine::Feature).to receive(:pass?).with(:ejbcaws_accessible).and_return(true)
-      allow(Ejbcaws::Client).to receive(:version).and_return('EJBCA 6.10.1.2 Community')
+      allow(Puppet.features).to receive(:ejbcaws_accessible?).and_return(true)
+      allow(Ejbcaws::Client).to receive(:version).and_return('EJBCA 6.10.1.2 Community (r27920)')
       expect(Facter.fact(:ejbcaversion).value).to eql('6.10.1.2')
     end
   end
